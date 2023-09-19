564d0838967028523b4d03973269166968cf634b
Eliott Brard <brarde71@gmail.com>
2023-09-19 18:10:07 +0200

    nouvelle fonction

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
 document.getElementById('title').innerText += ' from JS';
 document.getElementById('title').style.color = 'red';
+
+changeTitle();
+
+function changeTitle(){
+    const title=document.getElementById("title");
+    title.innerText += "from js";
+    title.style.color += "red";
+}
